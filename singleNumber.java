1d0e9fb419b61f811dfb35e1ecc41cc735ea0fa9
dinhh <dinh4512@cs.utexas.edu>
2015-02-12 09:26:53 -0600

    started working of Repeating DNA

--- singleNumber.java
+++ singleNumber.java
@@ -1,8 +1,14 @@
 //Given an array of integers, every element appears twice except for one. 
 //Find that single one.
 
-pulbic class singleNum{
-	public int singleNumber(int[] A) {
+class singleNum{
+	public void main(String[] args) {
+		int[] num = {1,2,3,4,5,6,7,8,1,2,3,4,5,6,7};
+
+		singleNum(num);
+	}
+
+	public int singleNum(int [] A){
 		int ret = 0;
 		int i;
 		for (i = 0; i < A.length; i++) {
