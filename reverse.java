6761014ee043df4f58ac4ef1e8357832f6bbe82e
dinhh <dinh4512@cs.utexas.edu>
2015-02-19 09:21:46 -0600

    added new solution to ReverseStr

---
+++ reverse.java
@@ -0,0 +1,18 @@
+class Reverse{
+	
+	public String reverseStr( String str){
+		char[] chars = str.toCharArray();
+		for(int i = 0, j = str.length() - 1; i < j; i++, j--){
+			char temp = chars[i];
+			chars[i] = chars[j];
+			chars[j] = temp;
+		}
+		return new String(chars);
+	}
+
+	public void main(String[] args){
+		 Reverse obj = new Reverse();
+		 System.out.println(obj.reverseStr("hello world"));
+         System.out.println(obj.reverseStr("mihir"));
+	}
+}
