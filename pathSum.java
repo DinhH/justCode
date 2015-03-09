61a2c173e0e33c7459598d317fcb1c2cce6993e4
dinhh <dinh4512@cs.utexas.edu>
2015-03-09 14:36:44 -0500

    added solution

--- pathSum.java
+++ pathSum.java
@@ -1,1 +1,14 @@
 //given a binary tree and a sum, determine if the tree has a root-to-leaf path such that adding up all the values along the path equals the given sum.
+
+class Solution{
+public boolean pathSum(TreeNode root, int sum) {
+	if (root == null){
+		return false;
+	}
+	if (root.val == sum && (root.left == null && root.right == null)){
+		return true;
+	}
+	return pathSum(root.left, sum - root.val)
+			|| pathSum(root.right, sum - root.val);
+	}
+}
