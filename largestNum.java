e646fb8ced7ecb848b0a11c085d3e8d182996bea
dinhh <dinh4512@cs.utexas.edu>
2015-03-17 17:13:05 -0400

    trying to finish largest number

--- largestNum.java
+++ largestNum.java
@@ -9,7 +9,25 @@ public String largestNum(int[] num){
 	int i;
 	for(i = 0; i <num.length; i++){
 		ret[i] = String.valueOf(num[i]);
-
+	}
 	java.util.Arrays.sort(ret, new java.util.Comparator,String>() {
-		StringRL = right.concat(right);
-		
+		public int compare(String right, left){
+			String rL = right.concat(right);
+			String lR = left.concat(left);
+			return rl.compareTo(lR);
+		}
+	});
+	//trying to append the number together after comparasion
+	StringBuilder newString = new StringBuilder();
+	int i;
+	for (i = 0; i < ret.length; i++){
+		newString.append(ret[i]);
+	}
+	
+	//picking the larger of the options
+	//LeetCode suggested using BigInteger
+	java.math.BigInteger ret1 = new java.math.BigInteger(newString.toString());
+	return ret1.toString();
+}
+
+	
