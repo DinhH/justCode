e0b628112853b1ef3aa23c9eea7d1f9459b70f2b
dinhh <dinh4512@cs.utexas.edu>
2015-03-20 14:19:52 -0500

    add part solution for divide

--- divide.java
+++ divide.java
@@ -4,4 +4,17 @@
 
 public class Solution{
 	public int divide(int dividend, int divisor){
-	
+		int ret = 0;
+		
+		while(devidend >= divisor){
+			int counter = 0;
+			while(devidend >= (divisor << counter)){
+				counter++;
+			}
+			ret += 1 << (counter -1);
+			dividend -= << (counter -1);
+		}
+		if (dividend == Integer.MIN_VALUE && divisor == -1){
+			//return the larger of the two
+		}
+			
