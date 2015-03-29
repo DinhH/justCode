0aca93ebf20db3b2a7b0558c37800b46601db57f
dinhh <dinh4512@cs.utexas.edu>
2015-03-28 23:18:32 -0500

    some of hamming weight

--- hammingWeight.java
+++ hammingWeight.java
@@ -2,3 +2,13 @@
 //For example, the 32-bit integer ’11' has binary representation 00000000000000000000000000001011, so the function should return 3.
 
 
+
+public int hammingWeight(int n) {
+         int count=0;
+        while(n!=0){
+            n=n&(n-1);
+            count++;
+        }
+        return count;
+    }
+
