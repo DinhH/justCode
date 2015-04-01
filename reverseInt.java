284b4a2656c7e3f3e5bd61efb0b7acafcd39e2fd
dinhh <dinh4512@cs.utexas.edu>
2015-03-31 21:16:25 -0500

    work with my little brothergit add -A

--- reverseInt.java
+++ reverseInt.java
@@ -2,3 +2,25 @@
 
 //Example1: x = 123, return 321
 //Example2: x = -123, return -321
+
+public int reverseInt(int num){
+	boolean fg = false;
+	if(num < 0 ){
+		num = 0 - num;
+		fg = true;
+	}
+	int res = 0;
+	int newNum = num;
+	
+	while (newNum > 0){
+		int mod = newNum % 10;
+		newNum = newNum/10;
+		res = res * 10 + mod;
+	}
+	if(fg){
+		res = 0 - res;
+	}
+	
+	return res;
+}
+
