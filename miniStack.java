279da802969de4977f7cbc83578f1c668093e748
dinhh <dinh4512@cs.utexas.edu>
2015-04-02 22:53:30 -0500

    push

--- miniStack.java
+++ miniStack.java
@@ -4,3 +4,12 @@
 //pop() -- Removes the element on top of the stack.
 //top() -- Get the top element.
 //getMin() -- Retrieve the minimum element in the stack.
+
+
+void push(int x) {
+        s.push(x);
+        if (sMin.empty() || x < sMin.top())
+            sMin.push(x);
+        else
+            sMin.push(sMin.top());
+
