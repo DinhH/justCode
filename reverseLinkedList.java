776d3704945855f9b545ca874083fee0f6a3c5a0
dinhh <dinh4512@cs.utexas.edu>
2015-04-05 18:15:04 -0500

    added portion of reverse Linked list

--- reverseLinkedList.java
+++ reverseLinkedList.java
@@ -6,3 +6,18 @@ Given 1->2->3->4->5->NULL, m = 2 and n = 4,
 return 1->4->3->2->5->NULL.*/
 
 
+puublic class Solution{
+	public void reverse(ListNode pre, ListNode endNext){
+        ListNode cur=pre.next;
+        
+        while (cur.next!=endNext){
+            
+            ListNode next=cur.next;
+            ListNode temp=pre.next;
+            pre.next=next;
+            cur.next=next.next;
+            
+            next.next=temp;
+            
+        }
+    }
