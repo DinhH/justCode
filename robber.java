2c3bee66708cc6bc3d6da94b2227f1907c58f9f5
dinhh <dinh4512@cs.utexas.edu>
2015-04-13 16:59:03 -0500

    added solution to home Robber

--- robber.java
+++ robber.java
@@ -3,4 +3,36 @@
 them is that adjacent houses have security system connected and it will automatically contact the police if two adjacent houses were broken into on the same night.
 Given a list of non-negative integers representing the amount of money of each house, determine the maximum amount of money you can rob tonight without alerting the police.*/
 
+public int robber (int[] num){
+	if(num == null || num.lenghth == 0){
+		return 0;
+	}
+	
+	int odd = 0;
+	int even = 0;
 
+	for(int i = 0; i < num.length; i++){
+		if(i%2 == 0){
+			even += num[i];
+			if(even > odd){
+				even = even;
+			}
+			else{
+				even = odd;
+		}
+		else{
+			odd += num[i];
+			if(even > odd){
+				odd = even;
+			}
+			else{
+				odd = odd;
+			}
+		}
+	if(even > odd){
+		return even;
+	}
+	else{
+		return odd;
+	}
+}
